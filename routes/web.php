f193f028122a6cd2646abac2a44e8d516cec743e
shathaaboyousef <shathaaboyo18@gmail.com>
2025-02-16 14:34:47 +0200

    second project

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about',function(){
-    $name="hala";
+Route::get('/about', function () {
+    $name = "hala";
     $departments = [
         '1' => 'Technical',
         '2' => 'Financial',
         '3' => 'Sales'
     ];
-    return view('about',compact('name', 'departments'));
+    return view('about', compact('name', 'departments'));
 });
-Route::post('/about',function(){
-    $name=$_POST['name'];
-    $departments= [
-        '1'=>'Technical',
-        '2'=>'Financial',
-        '3'=>'Sales'
+Route::post('/about', function () {
+    $name = $_POST['name'];
+    $departments = [
+        '1' => 'Technical',
+        '2' => 'Financial',
+        '3' => 'Sales'
     ];
-    return view('about',compact('name' , 'departments'));
+    return view('about', compact('name', 'departments'));
+});
+Route::get('tasks', function () {
+    return view('tasks');
+});
+Route::post('create', function () {
+    $task_name = $_POST['name'];
+    DB::table('tasks')->insert(['name' => $task_name]);
+    return view('tasks');
 });
